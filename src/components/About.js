36a0e4015315142cf9076a62e0b28421b2151945
rails <shubham.tiwari2911@gmail.com>
2018-09-13 17:55:05 +0530

    basic react-redux

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow'
+
+const About=()=>{
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias sequi non fuga, quia facilis ipsa illum at ducimus corporis! Iusto, distinctio aut totam eaque provident ipsam error quam doloremque quidem?</p>
+
+        </div>
+    )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact=(props)=>{
+    // setTimeout(()=>{
+    //     props.history.push('/about')
+    // },2000)
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias sequi non fuga, quia facilis ipsa illum at ducimus corporis! Iusto, distinctio aut totam eaque provident ipsam error quam doloremque quidem?</p>
+
+        </div>
+    )
+}
+
+export default Contact;
